e378b55a99879e8f459e7f36963ce2821b64030d
ComunidadColee <coleecomunidad@gmail.com>
2020-08-06 19:05:37 -0500

    comentario

---
+++ src/whilenumeros/WhileNumeros.java
@@ -0,0 +1,26 @@
+package whilenumeros;
+
+import java.util.Scanner;
+
+public class WhileNumeros {
+    
+    public static void main(String [] args){
+        Scanner entrada = new Scanner(System.in);
+        int n = 0;
+        
+        System.out.println("Escribe un numero positivo o negativo");
+        n = entrada.nextInt();
+        
+        while (n != 0){
+            if (n < 0){
+                double cdr = Math.pow(n, 2);
+                System.out.println("El cuadrado de " + n + " es " + cdr);
+            } else {
+                double cubo = Math.pow(n, 3);
+                System.out.println("El cubo de " + n + " es " + cubo);
+            }
+            System.out.println("Escibre otro numero");
+            n = entrada.nextInt();
+        }
+    }
+}
